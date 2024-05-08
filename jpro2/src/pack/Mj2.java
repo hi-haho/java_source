10cad71a6a11f9075ef9a53ecd9ce6998cfe5a17
sungha <from_you_me@naver.com>
2024-05-08 09:33:53 +0900

    문제풀이

--- jpro2/src/pack/Mj2.java
+++ jpro2/src/pack/Mj2.java
@@ -42,9 +42,6 @@ public class Mj2 extends JFrame implements ActionListener {
 		// 드라이버 로딩
 		try {
 			Class.forName("org.mariadb.jdbc.Driver");
-			
-			String url = "jdbc:mariadb://localhost:3306/test";
-			conn = DriverManager.getConnection(url, "root", "123");
 
 			display();
 
@@ -79,24 +76,26 @@ public class Mj2 extends JFrame implements ActionListener {
 					return;
 				}
 				try {
-					su = Integer.parseInt(txtC.getText());
+					su = Integer.parseInt(txtS.getText());
 				} catch (Exception e2) {
 					System.out.println("숫자를 입력하세요");
 					return;
 				}try {
-					dan = Integer.parseInt(txtC.getText());
+					dan = Integer.parseInt(txtD.getText());
 				} catch (Exception e2) {
 					System.out.println("숫자를 입력하세요");
 					return;
 				}
 				
+				String url = "jdbc:mariadb://localhost:3306/test";
+				conn = DriverManager.getConnection(url, "root", "123");
 				
-				/* 자료 추가 */// txtC,txtP, txtS, txtD (code,sang,su,dan)
-				sql = "insert into sangdata(code,sang,su,da) values(?,?,?,?)";
-				System.out.println("여ㅣ까지 정상");
+				/* 자료 추가 */// txtC,txtP, txtS, txtD
+				sql = "insert into sangdata values(?,?,?,?)";
+				//System.out.println(code + " " + txtP.getText() + " " + su + " " + dan);
+				
 				pstmt = conn.prepareStatement(sql);
-				System.out.println("여기까지 찍히몀ㄴ 까지 정상");
-				
+			
 				pstmt.setInt(1,code);
 				pstmt.setString(2, txtP.getText());
 				pstmt.setInt(3, su); 
@@ -190,3 +189,149 @@ public class Mj2 extends JFrame implements ActionListener {
 	}
 
 }
+
+/*
+ package pack;
+
+import java.awt.Event;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.text.DecimalFormat;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class DbTest07 extends JFrame implements ActionListener {
+	JTextField code = new JTextField("", 5);
+	JTextField name = new JTextField("", 10);
+	JTextField cnt = new JTextField("", 5);
+	JTextField price = new JTextField("", 5);
+	JButton plus = new JButton("추가");
+	JTextArea result = new JTextArea();
+	String sql = "";
+
+	Connection conn;
+	PreparedStatement pstmt;
+	ResultSet rs;
+
+	public DbTest07() {
+		super("상품추가 연습문제");
+
+		accDb();
+		layInit();
+
+		setBounds(200, 200, 600, 300);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	private void layInit() {
+
+		JPanel panel1 = new JPanel();
+		panel1.add(new JLabel("코드 : "));
+		panel1.add(code);
+		panel1.add(new JLabel("품명 : "));
+		panel1.add(name);
+		panel1.add(new JLabel("수량 : "));
+		panel1.add(cnt);
+		panel1.add(new JLabel("단가 : "));
+		panel1.add(price);
+		panel1.add(plus);
+		add("North", panel1);
+
+		
+		result.setEditable(false);
+		JScrollPane pane = new JScrollPane(result);
+		add(pane);
+
+		plus.addActionListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		try {
+			String co = code.getText();
+			String na = name.getText();
+			String cn = cnt.getText();
+			String pr = price.getText();
+
+			sql = "insert into sangdata values(?,?,?,?)";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, co);
+			pstmt.setString(2, na);
+			pstmt.setString(3, cn);
+			pstmt.setString(4, pr);
+			rs = pstmt.executeQuery();
+
+
+			display();
+
+		} catch (Exception e2) {
+			JOptionPane.showMessageDialog(this, "코드가 중복되었거나 입력되지 않았습니다.");
+		}
+	}
+
+	private void accDb() {
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			
+			processDb();
+		} catch (Exception e) {
+			System.out.println("accDb err : " + e);
+		}
+		display();
+		
+	}
+
+	private void processDb() {
+		try {
+			String url = "jdbc:mariadb://localhost:3306/test";
+			conn = DriverManager.getConnection(url, "root", "123");
+
+			display();
+
+		} catch (Exception e) {
+			System.out.println("processDb error : " + e);
+		}
+	}
+
+	private void display() {
+		try {
+			DecimalFormat df = new DecimalFormat("#,##0"); //#,### > 무효의 제료로 본다. #,##0 > 유효의 제로(0)로 본다
+			sql = "select * from sangdata";
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+
+			int count = 0;
+			result.setText("코드  \t 상품명  \t 수량  \t 단가  \t 금액  \n");
+			while (rs.next()) {
+				result.append(rs.getString("code") + "\t" + rs.getString("sang") + "\t" + rs.getString("su") + 
+						"\t" + rs.getString("dan") + "\t" + df.format(rs.getInt("su") * rs.getInt("dan")) + "\n");
+				count++;
+			}
+			result.append("건수 : " + count);
+		} catch (Exception e) {
+			System.out.println("display() error : " + e);
+		}
+	}
+
+
+	public static void main(String[] args) {
+		new DbTest07();
+
+	}
+
+}
+
+ * */
--- jpro2/src/pack/mj3ExGogek.java
+++ jpro2/src/pack/mj3ExGogek.java
@@ -1,23 +1,155 @@
 package pack;
 
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
-public class mjGogek extends JFrame implements ActionListener {
+public class mj3ExGogek extends JFrame implements ActionListener {
+    Connection  conn;
+    PreparedStatement pstmt;
+    ResultSet rs;
 
-	public mjGogek() {
-		
-	}
-	
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		
-		
-	}
-	public static void main(String[] args) {
-		new mjGogek();
-	}
+    JButton btnOk;
+    JLabel lblName, lbljum; // 고객명, 주민번호
+    JTextField txtName, txtJuF, txtJuL; //고객명, 주민앞, 주민뒤
+    JTextArea txtArea = new JTextArea();
 
+
+    public mj3ExGogek(){
+        super("담당자 찾기");
+
+        layoutInit();
+        adddb();
+
+        setBounds(200,300,500,300);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        try {
+            txtArea.setText(null);
+            String url="jdbc:mariadb://localhost:3306/mydb";
+            conn = DriverManager.getConnection(url,"root","123");
+
+            String sql = "";
+
+            //직원 정보
+            // 열이름 가져오기 txtName, txtJuF, txtJuL
+            sql = "select jikwon_no,jikwon_name,buser.buser_name, buser.buser_tel, jikwon_jik from jikwon ,buser where buser_no = buser_num and jikwon_no = (select gogek_damsano from gogek where gogek_damsano = (select jikwon_no from jikwon where gogek_damsano = jikwon_no and gogek_name = '박상운' and gogek_jumin = '790623-1023777'))";
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1,txtName.getText());
+            pstmt.setString(2,txtJuF.getText());
+            pstmt.setString(3,txtJuL.getText());
+            rs = pstmt.executeQuery();
+
+            //유효성 검사
+            int juminF,juminL;
+            String gogekN;
+            //입력 확인
+            if (txtName.getText().equals("")){
+                JOptionPane.showMessageDialog(this,"이름을 입력하세요.");
+                txtName.requestFocus();
+                return;
+            }
+            if (txtJuF.getText().equals("") ||txtJuL.getText().equals("")){
+                JOptionPane.showMessageDialog(this,"주민번호을 입력하세요.");
+                txtJuF.requestFocus();
+                return;
+            }
+            if (txtJuF.getText().length() != 6 ||txtJuL.getText().length() != 7 ){
+                JOptionPane.showMessageDialog(this,"주민 번호를 입력하세요.");
+                txtJuF.setText("");
+                txtJuL.setText("");
+                txtJuF.requestFocus();
+                return;
+            }
+            //주민번호 숫자 확인
+            try {
+            juminF = Integer.parseInt(txtJuF.getText());
+            juminL = Integer.parseInt(txtJuL.getText());
+            }catch (NumberFormatException e2){
+                JOptionPane.showMessageDialog(this,"주민 번호를 입력하세요.");
+                txtJuF.setText("");
+                txtJuL.setText("");
+                txtJuF.requestFocus();
+            }
+
+            //결과 출력
+            txtArea.append("담당 직원 정보\n");
+
+            if (rs.next()){ //jikwon_no,jikwon_name,buser.buser_name, buser.buser_tel, jikwon_jik
+                String imsi = rs.getString(1)+"\t"+
+                        rs.getString(2)+"\t"+
+                        rs.getString(3)+"\t"+
+                        rs.getString(4)+"\t"+
+                        rs.getString(5)+"\n";
+                txtArea.append(imsi);
+            }
+        }catch (Exception e2){
+            System.out.println("Db server conn err : "+e2);
+        }
+    }
+
+    private void layoutInit(){
+        // JLabel lblName, lbljum
+        // txtName, txtJuF, txtJuL; //고객명, 주민앞, 주민뒤
+        setLayout(new GridLayout(2,1));
+
+        JPanel panel1 = new JPanel();
+        lblName = new JLabel("이름: ");
+        txtName = new JTextField("",10);
+        panel1.add(lblName);
+        panel1.add(txtName);
+        add("N",panel1);
+
+        JPanel panel2 = new JPanel();
+        lbljum = new JLabel("주민번호: ");
+        txtJuF = new JTextField("",6);
+        txtJuL = new JTextField("",6);
+        btnOk = new JButton("확인");
+        panel2.add(lbljum);
+        panel2.add(txtJuF);
+        panel2.add(txtJuL);
+        panel2.add(btnOk);
+        add(panel2);
+        btnOk.addActionListener(this);
+
+        JPanel panel3 = new JPanel();
+        panel3.setLayout(new GridLayout(2,1));
+        panel3.add(panel1);
+        panel3.add(panel2);
+        add(panel3);
+
+        txtArea.setEditable(false);
+        add(txtArea);
+    }
+
+    private void adddb(){
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+
+        }catch (ClassNotFoundException e){
+            System.out.println("addDb err : "+ e);
+        }
+    }
+
+    public static void main(String[] args) {
+        new mj3ExGogek();
+    }
 }
